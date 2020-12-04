ee598818d9c25c07376c46b0949989fb3942387e
Newland-A <ajn252@gmail.com>
2020-12-04 07:39:27 -0500

    commit tree 09b89bb935393cf1bd358c3f74e972309bbe01c2

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your functions here
+function reduce(all, night, long) {
+  let result = (!!long) ? long : all[0]
+  let i = (!!long) ? 0 : 1
+  for(; i < all.length; i++) {
+    result = night(all[i], result)
+  } 
+  return result
+}
+
+function map(srcArray, element) {
+  let result = []
+  for(let i = 0; i < srcArray.length; i++) {
+    let array = srcArray[i]
+    result.push(element(array))
+  }
+  return result
+}
